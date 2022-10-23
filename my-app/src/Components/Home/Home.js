8e799dc89dbfde0699679370504400698abdc340
NeemoDab <neemodab@gmail.com>
2022-10-23 20:45:39 +0100

    continue tomorrow with structure of site and importing logo

---
+++ my-app/src/Components/Home/Home.js
@@ -0,0 +1,8 @@
+
+const Home = () => {
+return(
+    <h1>Zeus Energy Services</h1>
+)
+}
+
+export default Home
---
+++ my-app/src/Components/Logo/Logo.js
@@ -0,0 +1,9 @@
+//import logo
+const Logo = () => {
+    return(
+        <div></div>
+        // <img src="www.google.com" ></img>
+    )
+    }
+    
+    export default Logo
